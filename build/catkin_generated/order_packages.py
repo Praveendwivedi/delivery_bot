fa8f8c7a7d5ac57f7c620e28c798da13982929f9
Praveendwivedi <praveen98ok@gmail.com>
2020-08-20 21:10:02 +0530

    created urdf model and rviz launch file

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/praveen/delivery_bot/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/praveen/faltu/devel;/home/praveen/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/praveen/faltu/devel;/home/praveen/catkin_ws/devel;/opt/ros/noetic' != '' else []
